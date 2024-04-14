6da1e29cf7e6f4d7d2fd45237b96228a7e3a1a68
MorosiniP <pmorosini@frba.utn.edu.ar>
2024-04-14 22:08:09 +0000

    avances kernel

---
+++ kernel/src/kernel.h
@@ -0,0 +1,27 @@
+#ifndef KERNEL_H_
+#define KERNEL_H_
+
+#ifndef DEBUG 
+#define DEBUG 1
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <string.h>
+#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/string.h>
+#include <commons/collections/list.h>
+#include <commons/collections/dictionary.h>
+#include <utils/socket.h>
+#include <semaphore.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif /* KERNEL_H_ */
