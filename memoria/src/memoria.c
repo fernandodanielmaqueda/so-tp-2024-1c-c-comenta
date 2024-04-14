f766e8451fb2615d336afce0aaae0058809b43fc
MorosiniP <pmorosini@frba.utn.edu.ar>
2024-04-14 22:13:02 +0000

    avances memoria

---
+++ memoria/src/memoria.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/string.h>
+#include <commons/memory.h>
+#include <commons/bitarray.h>
+#include <commons/collections/list.h>
+#include <commons/collections/queue.h>
+#include <utils/socket.h>
+#include "memoria.h"
+
+int main(int argc, char* argv[]) {
+  
+    return 0;
+}
---
+++ memoria/src/memoria.h
@@ -0,0 +1,24 @@
+#ifndef MEMORIA_H_
+#define MEMORIA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/string.h>
+#include <commons/memory.h>
+#include <commons/bitarray.h>
+#include <commons/collections/list.h>
+#include <commons/collections/queue.h>
+#include <utils/socket.h>
+
+
+t_log* memoria_logger;
+t_log* memoria_debug_logger;
+t_config* memoria_config;
+
+
+#endif /* MEMORIA_H_ */
