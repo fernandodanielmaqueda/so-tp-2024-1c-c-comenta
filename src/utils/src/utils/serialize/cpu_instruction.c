65fb72d80e894e28dd3d0a34695af349c920ff44
Brai93 <brianfink@frba.utn.edu.ar>
2024-06-11 20:11:11 +0000

    SERIALIZADO de instruccion: Print

--- src/utils/src/utils/serialize/cpu_instruction.c
+++ src/utils/src/utils/serialize/cpu_instruction.c
@@ -52,48 +52,12 @@ t_CPU_Instruction *cpu_instruction_deserialize(t_Payload *payload) {
 }
 
 void cpu_instruction_print(t_CPU_Instruction *instruction) {
-    /*
-  log_info(SERIALIZE_LOGGER,
-    "t_CPU_Instruction[%p]:\n"
-    "* PID: %" PRIu32 "\n"
-    "* PC: %" PRIu32 "\n"
-    "* AX: %" PRIu8 "\n"
-    "* BX: %" PRIu8 "\n"
-    "* CX: %" PRIu8 "\n"
-    "* DX: %" PRIu8 "\n"
-    "* EAX: %" PRIu32 "\n"
-    "* EBX: %" PRIu32 "\n"
-    "* ECX: %" PRIu32 "\n"
-    "* EDX: %" PRIu32 "\n"
-    "* RAX: %" PRIu32 "\n"
-    "* RBX: %" PRIu32 "\n"
-    "* RCX: %" PRIu32 "\n"
-    "* RDX: %" PRIu32 "\n"
-    "* SI: %" PRIu32 "\n"
-    "* DI: %" PRIu32 "\n"
-    "* quantum: %" PRIu32 "\n"
-    "* current_state: %" PRIu8 "\n"
-    "* arrival_READY: %g\n"
-    "* arrival_RUNNING: %g"
-    ,(void *) instruction,
-    instruction->PID,
-    instruction->PC,
-    instruction->AX,
-    instruction->BX,
-    instruction->CX,
-    instruction->DX,
-    instruction->EAX,
-    instruction->EBX,
-    instruction->ECX,
-    instruction->EDX,
-    instruction->RAX,
-    instruction->RBX,
-    instruction->RCX,
-    instruction->RDX,
-    instruction->SI,
-    instruction->DI,
-    instruction->quantum,
-    instruction->current_state,
- 
-    */
+    log_info(SERIALIZE_LOGGER, "INSTRUCCION: %d\n", instruction->opcode);
+    int size = list_size(instruction->parameters);
+    
+    for (size_t i = 0; i < size; i++)
+    {
+      char * parametro = list_get(instruction->parameters,i);
+      log_info(SERIALIZE_LOGGER, "PARAMETRO [%ld]: %s :\n", i, parametro);
+    }
 }
